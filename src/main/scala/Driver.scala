a16e878999c8aeba2eb39a862808e55b4feb2df1
Seyedamirhossein Hesamian <seyedhesamian@northwesternmutual.com>
2020-04-14 22:53:08 -0500

    Minor

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -7,10 +7,13 @@ object Driver {
   def main(args: Array[String]): Unit = {
     val parser = new MipsParser
 
-    val source = scala.io.Source.fromFile(args.head)
+    val source = scala.io.Source.fromFile(args.headOption match {
+      case Some(value) => value
+      case None => "src/test.s"
+    })
     val code = try source.mkString finally source.close()
 
-    val instructions = parser.parseCode(new CharArrayReader(code.toArray, 0));
+    val instructions = parser.parseCode(new CharArrayReader(code.toArray, 0))
 
     println(instructions.mkString("\n"))
   }
